987b83cc6db2893158c4767294aec0cab3881ca9
Yucsan <yucsan2023@gmail.com>
2025-01-28 12:52:52 +0100

    sube

---
+++ bol1/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = { 
+    num1: 0
+   }
+  render() { 
+    return ( 
+      <div>
+        <h1>Prueba de Componentes</h1>
+
+      </div>
+
+     );
+  
+}
+ 
+
+}
+
+export default App;
---
+++ bol1/src/Componentes/Ejercicio1.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+export default class Ejercicio1 extends Component {
+  state = {
+    resultado: 0,
+    numerito1: 0,
+    numerito2: 0
+  }
+
+  recogeInput = (e) => {
+    var { id, value } = e.target;
+    this.setState({
+      [id]: Number(value) // los input devuelven cadenas
+    });
+  };
+
+
+  suma = () => {
+    var { numerito1, numerito2 } = this.state;
+    this.setState({
+      resultado: numerito1 + numerito2
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>Ejercicio Boletin 1 </h3>
+
+          <div>sumando1</div>
+          <input id="numerito1" type="number" value={this.state.numerito1} onChange={this.recogeInput} />
+          <div>sumando2</div>
+          <input id="numerito2" type="number" value={this.state.numerito2} onChange={this.recogeInput} />
+
+          <h1>{this.state.resultado} </h1>
+          <button onClick={this.suma} > Suma </button>
+        </header>
+      </div>
+    );
+  }
+
+
+}
